fc08aea3f29baef866f6376b732e69acdce6441b
Augusto Dantas <augustodantas.dev@gmail.com>
2024-04-07 16:43:15 -0300

    Create Login Form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import "./Login.css";
+
+import email_icon from "../assets/loginIcons/email.png";
+import password_icon from "../assets/loginIcons/password.png";
+
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login">
+      <main className="login-container">
+        <div className="login-header">
+          <h2 className="login-title">Login</h2>
+          <div className="underline"></div>
+        </div>
+        
+        <form className="login-form">
+          <label htmlFor="email-input">
+            <img src={email_icon} alt="Imagem de ícone de e-mail" />
+            <input type="text" id="email-input" placeholder="E-mail" />
+          </label>
+
+          <label htmlFor="password-input">
+            <img src={password_icon} alt="Imagem de ícone de senha" />
+            <input type="password" id="password-input" placeholder="Senha" />
+          </label>
+
+          <p className="forgot-password">Esqueceu a senha?<span> Clique aqui!</span></p>
+
+          <button className="btn-login" type="submit">Login</button>
+        </form>
+      </main>
+    </div>
   )
 }
 
